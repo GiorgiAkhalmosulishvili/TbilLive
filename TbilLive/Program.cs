be2c6b1145f6d54e979185ef491f67b68ccbfa5d
Giorgi Akhalmosulishvili <giorgi.akhalmosulisvhili1@gmail.com>
2025-11-30 01:39:17 +0400

    swagger api used in all envrionment

--- TbilLive/Program.cs
+++ TbilLive/Program.cs
@@ -28,11 +28,8 @@ var app = builder.Build();
 
 app.UseGlobalExceptionHandler();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
